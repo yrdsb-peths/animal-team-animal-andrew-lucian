253598cfed61521d6943a2868895254a2b44f744
KingBobSkibidi <lucianbai0808@gmail.com>
2026-05-12 12:03:15 -0400

    May 12: completed up to 1.9

--- Elephant.java
+++ Elephant.java
@@ -14,6 +14,25 @@ public class Elephant extends Actor
      */
     public void act()
     {
-        move(1);
+        if(Greenfoot.isKeyDown("left")){
+            move(-1);
+            move(-3);
+        }
+        else if(Greenfoot.isKeyDown("right")){
+            move(-1);
+            move(3);
+        }
+        
+        eat();
+    }
+
+    public void eat() {
+        if(isTouching(Apple.class)){
+            removeTouching(Apple.class);
+            MyWorld world = (MyWorld) getWorld();
+            world.createApple();
+            world.increaseScore();
+        }
+
     }
 }
--- MyWorld.java
+++ MyWorld.java
@@ -6,13 +6,36 @@ import greenfoot.*;
  * @version May 12, 2026
  */
 public class MyWorld extends World {
+    public int score = 0;
+    Label scoreLabel;
+    // constructor for objects of class MyWOrld
     public MyWorld() {
+        // create a new world
         super(600, 400, 1);
-        
+
+        // create elephant object
         Elephant elephant = new Elephant();
         addObject(elephant, 300, 200);
+        addObject(elephant, 300, 300);
         
+        // create a label
+        scoreLabel = new Label(0, 80);
+        addObject(scoreLabel, 50 , 50);
+
+        createApple();
+    }
+
+    // increase score
+    public void increaseScore(){
+        score++;
+        scoreLabel.setValue(score);
+    }
+    
+    // create new apple at random location at top
+    public void createApple(){
         Apple apple = new Apple();
-        addObject(apple, 300, 0);
+        int x = Greenfoot.getRandomNumber(600);
+        int y = Greenfoot.getRandomNumber(300);
+        addObject(apple, x, y);
     }
 }
